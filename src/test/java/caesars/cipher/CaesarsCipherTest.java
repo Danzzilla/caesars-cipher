ce9282d3e6ec10aaaf3a81369e5b58b6c2e26d84
Danzzilla <danielsvirida@gmail.com>
2020-12-17 15:15:20 -0800

    initial commit

---
+++ src/test/java/caesars/cipher/CaesarsCipherTest.java
@@ -0,0 +1,25 @@
+package caesars.cipher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CaesarsCipherTest {
+
+	private CaesarsCipher caesarsCipher = new CaesarsCipher();
+	
+	@Test
+	void testCipheredMessageWithOffsetOf12(){
+		assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 12));
+	}
+	
+	@Test
+	void testEmptyString(){
+		assertEquals("", caesarsCipher.cipher("", 12));
+	}
+
+//	@Test
+//	void testCipheredMessageWithOffsetOf11(){
+//		assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 11));
+//	}
+}
